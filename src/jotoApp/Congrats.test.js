c089d60c9ff554ae6f00855769358f52074adc3a
Saky96 <saigalsaksham96@gmail.com>
2020-05-30 11:51:53 +0530

    extract enzyme setup to setupTests.js

--- src/jotoApp/Congrats.test.js
+++ src/jotoApp/Congrats.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme'
 import checkPropTypes from 'check-prop-types';
 
 import Congrats from './Congrats';
 import { findByTestAttr, checkProps } from '../../test/testUtils';
 
-Enzyme.configure({adapter: new EnzymeAdapter()})
 const defaultProps = {success: false}
 /**
  * Factory function to create a ShallowWrapper for the App component.
