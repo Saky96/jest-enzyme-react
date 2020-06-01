4bc907c913c8c20c4eefc2e40dce1f6310e69f5c
Saky96 <saigalsaksham96@gmail.com>
2020-06-01 20:10:43 +0530

    Completed input render test for success=false

--- src/jotoApp/Input.js
+++ src/jotoApp/Input.js
@@ -3,16 +3,29 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
     render() {
+        const Contents = this.props.success ? null : (
+            <form className='form-inline'>
+                <input
+                    data-test = 'input-box' 
+                    className='mb-2 mx-sm-3'
+                    type = 'text'
+                    placeholder = 'enter guess'
+                />
+                <button data-test='submit-button' type='submit' className='btn btn-primary mb-2'>
+                    Submit
+                </button>
+            </form>
+        );
         return (
-            <div>
-                
+            <div data-test='component-input'>
+                {Contents}
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => {
-    return {};
+const mapStateToProps = ({success}) => {
+    return {success};
 }
 
 export default connect(mapStateToProps)(Input);
--- src/jotoApp/Input.test.js
+++ src/jotoApp/Input.test.js
@@ -11,16 +11,26 @@ const setup = (initialState ={}) => {
 }
 
 describe('render', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        const initialState = {success: false};
+        wrapper = setup(initialState)
+    })
+
     describe('word has not been guessed', () => {
         test('should render component without error', () => {
-            
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1)
         });
 
         test('should render input box', () => {
-            
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(1)
         });
         test('should render submit button', () => {
-            
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(1)
         });
     });
 
