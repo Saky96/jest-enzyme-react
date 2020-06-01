7930179b96dedd5742a13776d9e25526b01b6ab0
Saky96 <saigalsaksham96@gmail.com>
2020-06-01 20:16:27 +0530

    Completed input render test for success=false

--- src/jotoApp/Input.test.js
+++ src/jotoApp/Input.test.js
@@ -12,13 +12,12 @@ const setup = (initialState ={}) => {
 
 describe('render', () => {
 
-    let wrapper;
-    beforeEach(() => {
-        const initialState = {success: false};
-        wrapper = setup(initialState)
-    })
-
     describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = {success: false};
+            wrapper = setup(initialState)
+        })
         test('should render component without error', () => {
             const component = findByTestAttr(wrapper, 'component-input');
             expect(component.length).toBe(1)
@@ -35,15 +34,23 @@ describe('render', () => {
     });
 
     describe('word has been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = {success: true};
+            wrapper = setup(initialState)
+        })
         test('should render component without error', () => {
-            
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1)
         });
 
         test('should not render input box', () => {
-            
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(0)
         });
         test('should not render submit button', () => {
-            
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(0)
         });
     });
 });
