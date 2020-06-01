077ce37b4389137b605a520ce1da9d87ff1824ce
Saky96 <saigalsaksham96@gmail.com>
2020-06-01 17:31:59 +0530

    Codded and wrote tests for successReducer

--- src/jotoApp/actions/index.test.js
+++ src/jotoApp/actions/index.test.js
@@ -1,8 +1,8 @@
-import {correctGuess} from './';
+import {correctGuess, actionTypes} from './';
 
 describe('correctGuess actions test group', () => {
     test('should return object with type "CORRECT_GUESS"', () => {
         const action = correctGuess();
-        expect(action).toEqual({})
+        expect(action).toEqual({type: actionTypes.CORRECT_GUESS})
     });
 });
