6b7e087154b0d60cf4814c07a211fbd2973b6c4a
Saky96 <saigalsaksham96@gmail.com>
2020-06-02 14:40:17 +0530

    setup guessedWords.

---
+++ src/jotoApp/integration.test.js
@@ -0,0 +1,26 @@
+import { storeFactory } from '../../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+    describe('No guessed words', () => {
+        test('should update state correctly for unsuccessful guess', () => {
+            
+        });
+        
+        test('should update state correctly for successful guess', () => {
+            
+        });
+    });
+
+    describe('Some guessed words', () => {
+        test('should update state correctly for unsuccessful guess', () => {
+            
+        });
+        
+        test('should update state correctly for successful guess', () => {
+            
+        });
+    });
+
+
+});
---
+++ src/jotoApp/reducers/guessedWordsReducer.js
@@ -0,0 +1,10 @@
+
+/**
+ * @function guessWordsReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {array} - new guessed word states.
+ */
+export default(state, action) => {
+    return null;
+}
