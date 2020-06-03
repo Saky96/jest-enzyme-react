ed8d5c0bbdd921f01d112ae616f358f75026400e
Saky96 <saigalsaksham96@gmail.com>
2020-06-03 13:14:26 +0530

    Added secret word action creator and reducer.

--- src/jotoApp/reducers/secretWordReducer.js
+++ src/jotoApp/reducers/secretWordReducer.js
@@ -1,3 +1,18 @@
+import { actionTypes } from '../actions';
+
+/**
+ * @function secretWordReducer
+ * @param {string} state - state before reducer.
+ * @param {object} action - Action sent to reducer.
+ * @returns - new state (secretWord payload dispatched from action).
+ */
 export default (state=null, action) => {
+
+    switch (action.type) {
+        case actionTypes.SET_SECRET_WORD:
+            return action.payload 
+        default:
+            return state
+    }
     return state
 }
